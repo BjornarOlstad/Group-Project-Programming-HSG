b1040b78baa6e1f1417f6414a729afa5723faeeb
BjornarOlstad <s203804@nhhs.no>
2024-11-24 11:00:16 +0100

    update merge

---
+++ Merge_example.py
@@ -0,0 +1,10 @@
+def task_name():
+
+    print("Task Name: A descriptive name for your task.")
+    
+    # Place your code here.
+    # Ensure the code runs independently within this function.
+    
+    
+    print("Task completed successfully.\n")
+
