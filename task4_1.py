139ad6c0485e67ee2167d109f1021c7cd8b074e8
Floori2612 <phil-florens.tschoepe@gmx.net>
2024-11-28 23:42:46 +0100

    Update task4_1.py

--- task4_1.py
+++ task4_1.py
@@ -1,1 +1,8 @@
-
+def task1():
+    """
+    Task 1: Data Analysis and Visualization.
+    This task allows the user to select a region and a state to analyze data,
+    and provides visualizations such as bar and pie charts.
+    """
+    
+    print("Task 1: Data Analysis and Visualization")
